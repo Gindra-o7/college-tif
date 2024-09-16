da7a936ba2349a371959558a199a58fd8d47b371
hzqula <ilooqstrasi@gmail.com>
2024-09-17 06:38:54 +0700

    🔥 feat: nambahin bg-pattern dan nyoba mapping kursus tapi ga jalan

--- src/layouts/Hero.jsx
+++ src/layouts/Hero.jsx
@@ -1,16 +1,39 @@
 import React from "react";
-
-import { FaLaravel } from "react-icons/fa6";
-import { FaHandshakeAngle } from "react-icons/fa6";
 import CourseNav from "../components/CourseNav";
 
+import { FaLaravel, FaNodeJs } from "react-icons/fa6";
+import { FaHandshakeAngle } from "react-icons/fa6";
+
 const Hero = () => {
+  const listKursus = [
+    {
+      icon: { FaLaravel },
+      color: "red",
+      title: "Laravel",
+    },
+    {
+      icon: { FaNodeJs },
+      color: "green",
+      title: "NodeJS",
+    },
+  ];
+
   return (
-    <main className="flex items-center justify-center w-screen gap-4 p-24 mt-24">
-      <div className="flex items-center justify-between w-[40%] px-12 py-6 bg-ownWhite custom-border">
+    <main
+      className="relative flex items-center justify-center w-screen h-screen gap-4 p-24 before:bg-pattern before:bg-center before:bg-repeat before:absolute before:top-32 before:left-24 before:right-24 before:bottom-24
+    before:rounded-[6rem]"
+    >
+      <div className="relative flex items-center justify-between w-[40%] px-12 py-6 bg-ownWhite custom-border">
+        {listKursus.map((kursus, index) => {
+          <CourseNav
+            icon={kursus.icon}
+            color={kursus.color}
+            title={kursus.title}
+          />;
+        })}
         <CourseNav icon={FaLaravel} color="red" title="Laravel" />
       </div>
-      <div className="w-[60%] px-12 py-8 bg-ownWhite custom-border-lg font-trap">
+      <div className="relative w-[60%] px-12 py-8 bg-ownWhite custom-border-lg font-trap">
         <h2 className="text-2xl font-semibold text-secondary">
           Mari belajar dengan kami!
         </h2>
